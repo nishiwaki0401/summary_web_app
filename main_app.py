e8712c24ee1b8f2f28a2f67f6b8863b21d381500
nishiwaki0401 <hirohiropon0401@gmail.com>
2023-11-07 16:20:33 +0900

    first commit

---
+++ main_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title('要約アプリ')
+st.caption('愛媛新聞の要約されたものがみれます。')
+
+#テキストボックス
+input = st.text_input('要約したい文章')
+
+#ボタン
+submit_btn = st.button('送信')
---
+++ pages/page_1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title('要約アプリ')
+st.caption('過去の愛媛新聞の記事の要約されたものがあります。')
