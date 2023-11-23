b1086a6d81ee659aec2ad81eacf1525cb5089d32
nishiwaki0401 <150115004+nishiwaki0401@users.noreply.github.com>
2023-11-23 22:39:47 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -28,18 +28,24 @@ def init_messages():
         st.session_state.costs = []
     
 def select_model():
-    # サイドバーにオプションボタンを設置
     model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
     if model == "GPT-3.5":
         model_name = "gpt-3.5-turbo"
     else:
         model_name = "gpt-4"
 
-    # サイドバーにスライダーを追加し、temperatureを0から2までの範囲で選択可能にする
-    # 初期値は0.0、刻み幅は0.1とする
+    # スライダーを追加し、temperatureを0から2までの範囲で選択可能にする
+    # 初期値は0.0、刻み幅は0.01とする
     temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
 
-    return ChatOpenAI(temperature=temperature, model_name=model_name, streaming=True)
+    return ChatOpenAI(temperature=temperature, model_name=model_name)
+
+
+def get_answer(llm, messages):
+    with get_openai_callback() as cb:
+        answer = llm(messages)
+    return answer.content, cb.total_cost
+
 
 
 def main():
@@ -47,6 +53,14 @@ def main():
 
     llm = select_model()
     init_messages()
+
+    # ユーザーの入力を監視
+    if user_input := st.chat_input("聞きたいことを入力してね！")
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("ChatGPT is typing ..."):
+            answer, cost = get_answer(llm, st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=answer))
+        st.session_state.costs.append(cost)
 
     messages = st.session_state.get('messages', [])
     for message in messages:
@@ -59,15 +73,11 @@ def main():
         else:  # isinstance(message, SystemMessage):
             st.write(f"System message: {message.content}")
 
-    # ユーザーの入力を監視
-    user_input = st.chat_input("聞きたいことを入力してね！")
-    if user_input:
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        st.chat_message("user").markdown(user_input)
-        with st.chat_message("assistant"):
-            st_callback = StreamlitCallbackHandler(st.container())
-            response = llm(messages, callbacks=[st_callback])
-        st.session_state.messages.append(AIMessage(content=response.content))
+    costs = st.session_state.get('costs', [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
 
 if __name__ == '__main__':
     main()
